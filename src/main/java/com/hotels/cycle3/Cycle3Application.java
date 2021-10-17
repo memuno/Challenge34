6e53386401a386bc71eb6fb6071fa011043ba485
Mario Gomez <mariorobingomez@gmail.com>
2021-10-17 17:43:59 -0500

    Repositorio-limpio_SpringBoot_2.5.5

---
+++ src/main/java/com/hotels/cycle3/Cycle3Application.java
@@ -0,0 +1,13 @@
+package com.hotels.cycle3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cycle3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cycle3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotels/cycle3/Cycle3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotels.cycle3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cycle3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
